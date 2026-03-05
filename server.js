0246509c28dbe709ab9b721dd2ff6bdb0ecc7b52
Swastik1009 <140280775+Rohit100904@users.noreply.github.com>
2026-03-05 21:15:01 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
    🔥 FIREBASE INIT
 ============================== */
 
-const serviceAccount = require("./serviceAccount.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
